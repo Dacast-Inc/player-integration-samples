da79f8c045393917ff92e26b3d5fed782f08da3e
michael-elliott <michael.elliott@dacast.com>
2020-12-24 14:13:21 +0000

    ME: add Uapp support to iOS SDK

--- ios/DacastPlayer.swift
+++ ios/DacastPlayer.swift
@@ -1,24 +1,47 @@
 import Foundation
 import THEOplayerSDK
+
 class ContentId{
-    var broadcasterId: Int
-    var mediaId: Int
-    var contentType: String
-    
+    var provider: String = ""
+    var infoUrl: String = ""
+    var tokenUrl: String = ""
+    var broadcasterId: Int = 0
+    var mediaId: Int = 0
+    var mediaString: String = ""
+    var contentType: String = ""
+    var userId: String = ""
+    var contentId: String = ""
+}
+
+class DacastContentId: ContentId{
     init(contentIdStr: String) {
+        super.init()
         let split = contentIdStr.split(separator: "_")
         
         self.broadcasterId = Int(split[0])!
         self.mediaId = Int(split[2])!
         self.contentType = String(split[1])
-    }
-    
-    
-    func toUrl() -> String{
-        return String(self.broadcasterId) + "/" + self.contentType + "/" + String(self.mediaId)
+        self.provider = "dacast"
+        self.infoUrl = "https://json.dacast.com/b/" + String(self.broadcasterId) + "/" + self.contentType + "/" + String(self.mediaId)
+        self.tokenUrl = "https://services.dacast.com/token/i/b/" + String(self.broadcasterId) + "/" + self.contentType + "/" + String(self.mediaId) 
     }
 }
-​
+
+class UniverseContentId: ContentId{
+    init(contentIdStr: String) {
+        super.init()
+        //contentIdStr: 486dee4e-63b8-b2dc-3de3-5e6290a08281-vod-486dee4e-63b8-b2dc-3de3-5e6290a08281
+        self.contentId = contentIdStr
+        let split = contentIdStr.split(separator: "-")
+        self.userId = [String(split[0]), String(split[1]), String(split[2]), String(split[3]), String(split[4])].joined(separator: "-")
+        self.contentType = String(split[5])
+        self.mediaString = [String(split[6]), String(split[7]), String(split[8]), String(split[9]), String(split[10])].joined(separator: "-")
+        self.provider = "universe"
+        self.infoUrl = "https://playback.dacast.com/content/info?contentId="+contentId+"&provider=universe"
+        self.tokenUrl = "https://playback.dacast.com/content/access?contentId="+contentId+"&provider=universe"
+    }
+}
+
 extension UIImageView {
     func downloadedFrom(url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
         contentMode = mode
@@ -39,7 +62,7 @@ extension UIImageView {
         downloadedFrom(url: url, contentMode: mode)
     }
 }
-​
+
 public class DacastPlayer{
     
     public var frame: CGRect{
@@ -53,16 +76,14 @@ public class DacastPlayer{
             self.watermarkImg.frame = CGRect(x: value.minX + 5, y: value.minY + 5, width: value.width * 0.3, height: value.height * 0.3)
         }
     }
-    
+
     let theoplayer: THEOplayer
     let view: UIView
     let watermarkImg: UIImageView
     let adUrl: String?
-    
+        
     public init(contentIdStr: String, adUrl: String? = nil) {
         self.adUrl = adUrl
-        
-        let contentId = ContentId(contentIdStr: contentIdStr)
         
         view = UIView()
         
@@ -75,7 +96,7 @@ public class DacastPlayer{
         watermarkImg.alpha = 0.3
         view.insertSubview(watermarkImg, at: 9990000)
         
-        populatePlayerInfo(contentId: contentId)
+        populatePlayerInfo(contentIdStr: contentIdStr)
     }
     
     public func addAsSubview(of: UIView){
@@ -90,44 +111,96 @@ public class DacastPlayer{
         return theoplayer
     }
     
+    private func guessProviderFromContentId(contentId: String) -> String {
+        if (contentId.firstIndex(of: "-") != nil) {
+            return "universe"
+        } else if (contentId.range(of: #"\d+_(f|c|l|p)_\d+"#, options: .regularExpression) != nil) {
+            return "dacast"
+        }
+        else {
+            return "null"
+        }
+    }
+    
     private func toString(_ value: Any?) -> String {
       return String(describing: value ?? "")
     }
     
-    private func populatePlayerInfo(contentId: ContentId){
-        makeGetCall(endpoint: "https://json.dacast.com/b/" + contentId.toUrl()){
+    private func populatePlayerInfo(contentIdStr: String){
+        
+        let contentProvider : String = guessProviderFromContentId(contentId: contentIdStr)
+        
+        var contentId: ContentId
+        if (contentProvider == "universe") {
+            contentId = UniverseContentId(contentIdStr: contentIdStr)
+        }
+        else if(contentProvider == "dacast") {
+            contentId = DacastContentId(contentIdStr: contentIdStr)
+        }
+        else {
+            print("Defaulting")
+            contentId = ContentId()
+        }
+        
+        print(contentId)
+        
+        makeGetCall(endpoint: contentId.infoUrl){
             jsonData in
             
-            self.makeGetCall(endpoint: "https://services.dacast.com/token/i/b/" + contentId.toUrl()){
+            self.makeGetCall(endpoint: contentId.tokenUrl){
                 serviceData in
                 
-                var m3u8Link: String = jsonData["hls"] as! String
-                m3u8Link += serviceData["token"] as! String
-                let streamData = jsonData["stream"] as! [String : Any]
-                let splashLink = streamData["splash"] as! String
-                let themeData = jsonData["theme"] as! [String : Any]
-                let watermarkData = themeData["watermark"] as! [String : Any]
-                let watermarkLink = watermarkData["url"]
-                let subTitles = jsonData["subtitles"]
+                var splashLink: String
+                var watermarkLink: Any?
+                var textTracks: [TextTrackDescription] = []
+                var m3u8Link: String
                 
-                var textTracks : [TextTrackDescription] = []
-                for subTitle in subTitles as! [Dictionary<String,AnyObject>]
-                {
-                    let source = self.toString(subTitle["src"])
-                    let language = self.toString(subTitle["language"])
-                    let name = self.toString(subTitle["name"])
+                if (contentId.provider == "dacast") {
+                    m3u8Link = jsonData["hls"] as! String
+                    m3u8Link += serviceData["token"] as! String
+                    let streamData = jsonData["stream"] as! [String : Any]
+                    splashLink = streamData["splash"] as! String
+                    let themeData = jsonData["theme"] as! [String : Any]
+                    let watermarkData = themeData["watermark"] as! [String : Any]
+                    watermarkLink = watermarkData["url"]
+                    let subTitles = jsonData["subtitles"]
                     
-                    let textTrack = TextTrackDescription(src: source, srclang: language, label: name)
-                    textTracks.append(textTrack)
+                    for subTitle in subTitles as! [Dictionary<String,AnyObject>]
+                    {
+                        let source = self.toString(subTitle["src"])
+                        let language = self.toString(subTitle["language"])
+                        let name = self.toString(subTitle["name"])
+                        
+                        let textTrack = TextTrackDescription(src: source, srclang: language, label: name)
+                        textTracks.append(textTrack)
+                    }
                 }
-                
+                else {
+                    m3u8Link = serviceData["hls"] as! String
+                    let contentInfo = jsonData["contentInfo"] as! [String : Any]
+                    splashLink = contentInfo["splashscreenUrl"] as! String
+                    let featuresData = contentInfo["features"] as! [String : Any]
+                    let watermarkData = featuresData["watermark"] as! [String : Any]
+                    watermarkLink = watermarkData["imageUrl"]
+                    
+                    let subTitles = featuresData["subtitles"]
+                    for subTitle in subTitles as! [Dictionary<String,AnyObject>]
+                    {
+                        let source = self.toString(subTitle["sourceVtt"])
+                        let language = self.toString(subTitle["languageShortName"])
+                        let name = self.toString(subTitle["languageLongName"])
+                        
+                        let textTrack = TextTrackDescription(src: source, srclang: language, label: name)
+                        textTracks.append(textTrack)
+                    }
+                }
                 
                 DispatchQueue.main.async {
                     if !(watermarkLink is NSNull) && watermarkLink != nil {
                         self.watermarkImg.downloadedFrom(link: "https:" + (watermarkLink as! String))
                     }
                     
-                           
+                        
                     let typedSource = TypedSource(src: m3u8Link, type: "application/x-mpegurl")
                     //let analytics = ()
                     
@@ -141,6 +214,7 @@ public class DacastPlayer{
                 }
             }
         }
+    
     }
     
     private func makeGetCall(endpoint: String, onCompletion: @escaping ([String : Any]) -> ()) {
@@ -177,7 +251,6 @@ public class DacastPlayer{
                         return
                 }
                 onCompletion(parsedJson)
-​
             } catch  {
                 print("error trying to convert data to JSON")
                 return
@@ -185,6 +258,4 @@ public class DacastPlayer{
         }
         task.resume()
     }
-    
-    
 }
